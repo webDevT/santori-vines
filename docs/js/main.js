92cdb26fa82caabf82b8e343d73940653dd952f6
karinadesyatnik <karinadesyatnik27@gmail.com>
2021-06-25 18:01:11 +0300

    contact page

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
 
-
-
+//start mobil menu open-close
+$('.menu__btn').click(function(){
+	$(this).toggleClass('active');
+	$('.menu').toggleClass('active');
+})
+//end mobil-menu open-close
 });
